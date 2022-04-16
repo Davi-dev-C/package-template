e5a8ea5e015b481a272ebbc82da6f5151bb457e0
Davi h <54289647+Davi-dev-C@users.noreply.github.com>
2022-04-16 09:23:58 -0300

    implementa processamento de imagem

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
-    version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    name="image_processing2",
+    version="0.0.2",
+    author="Davi H.",
+    author_email="davi.henrique.lima01@gmail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Davi-dev-C/package-template",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
